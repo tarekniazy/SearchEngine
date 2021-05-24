654f41f7f98497b266ad199030deaa68db74d4ce
Khaled Ali Shaalan <khaled.shaalan.031@gmail.com>
2021-05-24 18:01:06 +0200

    Added DatabaseManager class

---
+++ src/main/java/indexer/DatabaseManager.java
@@ -0,0 +1,81 @@
+package indexer;
+
+import com.mongodb.*;
+import java.net.UnknownHostException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.util.*;
+
+import javax.print.Doc;
+
+public class DatabaseManager {
+
+    MongoClientURI uri;
+    MongoClient mongoClient;
+    DB database;
+    DBCollection collection;
+
+    DatabaseManager() {
+        uri = new MongoClientURI("mongodb+srv://admin:Khaled1999@cluster0.5toq8.mongodb.net/myDatabase?retryWrites=true&w=majority");
+
+        mongoClient = new MongoClient(uri);
+        database = mongoClient.getDB("myDatabase");
+        collection = database.getCollection("indexer");
+    }
+
+    public static void main( String args[] ) {
+
+
+        DatabaseManager db = new DatabaseManager();
+
+        db.insertDocument("wow", "twitter.com", 50);
+    }
+
+    public void insertDocument(String word, String url, int nf) {
+
+        DBObject query = new BasicDBObject("word", word);
+        DBCursor cursor = collection.find(query);
+
+        if(cursor.one() == null) //word will be inserted for the first time
+        {
+            JSONObject json = createJSON(url, nf);
+
+            DBObject dbObject = new BasicDBObject().append("word", word).append("data", json);
+
+            collection.insert(dbObject);
+        }
+        else { //word exists, incrementally update it
+            DBObject result = cursor.next();
+
+            BasicDBList obj = (BasicDBList) result.get("data");
+
+            JSONObject json = createJSON(url, nf);
+            obj.add(json);
+
+            collection.update(new BasicDBObject("word", word), new BasicDBObject("$set", new BasicDBObject("data", obj)));
+        }
+
+
+    }
+
+//    public void query(String word) {
+//
+//        DBObject query = new BasicDBObject("word", word);
+//        DBCursor cursor = collection.find(query);
+//        BasicDBList obj = (BasicDBList) cursor.one().get("data");
+//
+//    }
+
+    private JSONObject createJSON(String url, int NF) {
+        JSONObject json = new JSONObject();
+        json.put("NF", NF);
+        json.put("url", url);
+
+        return json;
+    }
+}
